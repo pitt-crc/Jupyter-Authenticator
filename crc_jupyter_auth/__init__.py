2e8c3b375335308a889e804a07578c835e061944
Daniel Perrefort <djperrefort@pitt.edu>
2022-08-08 13:29:26 -0400

    Add additional meta to setup.py file (#30)
    * Add additional meta to setup.py file
    
    * Update CI to use pip extras

--- crc_jupyter_auth/__init__.py
+++ crc_jupyter_auth/__init__.py
@@ -1,5 +1,7 @@
 """JupyterHub plugin for authenticating users and routing incoming HTTP requests."""
 
-__version__ = '0.3.0'
-
 from .remote_user_auth import RemoteUserAuthenticator, RemoteUserLocalAuthenticator
+
+__version__ = '0.3.0'
+__author__ = 'Pitt Center for Research Computing'
+__license__ = 'GNU GPL V3'
