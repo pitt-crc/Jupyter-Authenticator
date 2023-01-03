e43b1ecfeeb48cfa1391e7676743c7f5e1c0b34d
Daniel Perrefort <djperrefort@gmail.com>
2023-01-03 15:05:05 -0500

    Adds explicit Pythpon 3.7 support for __version__

--- crc_jupyter_auth/__init__.py
+++ crc_jupyter_auth/__init__.py
@@ -1,7 +1,13 @@
 """JupyterHub plugin for authenticating users and routing incoming HTTP requests."""
 
-import importlib.metadata
+import sys
 
 from .remote_user_auth import RemoteUserAuthenticator, RemoteUserLocalAuthenticator
 
-__version__ = importlib.metadata.version(__package__)
+if sys.version_info.minor <= 7:
+    import pkg_resources
+    __version__ = pkg_resources.get_distribution(__package__).version
+
+else:
+    import importlib.metadata
+    __version__ = importlib.metadata.version(__package__)
