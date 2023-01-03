74654cd76c66e084d0d50f4e7772276ba9074df3
Daniel Perrefort <djperrefort@pitt.edu>
2023-01-03 15:06:50 -0500

    Drops __version__ from init (#62)
    * Dynamically set __version__ from package metadata
    
    * Adds explicit Pythpon 3.7 support for __version__
    
    * Drops __ersion__ from init

--- crc_jupyter_auth/__init__.py
+++ crc_jupyter_auth/__init__.py
@@ -1,7 +1,3 @@
 """JupyterHub plugin for authenticating users and routing incoming HTTP requests."""
 
 from .remote_user_auth import RemoteUserAuthenticator, RemoteUserLocalAuthenticator
-
-__version__ = '0.5.1'
-__author__ = 'Pitt Center for Research Computing'
-__license__ = 'GNU GPL V3'
