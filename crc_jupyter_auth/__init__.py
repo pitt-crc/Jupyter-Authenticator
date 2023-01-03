428a27d5e8b5c2e1244e0951554c37fd5ee67b29
Daniel Perrefort <djperrefort@gmail.com>
2023-01-03 15:05:45 -0500

    Drops __ersion__ from init

--- crc_jupyter_auth/__init__.py
+++ crc_jupyter_auth/__init__.py
@@ -1,13 +1,3 @@
 """JupyterHub plugin for authenticating users and routing incoming HTTP requests."""
 
-import sys
-
 from .remote_user_auth import RemoteUserAuthenticator, RemoteUserLocalAuthenticator
-
-if sys.version_info.minor <= 7:
-    import pkg_resources
-    __version__ = pkg_resources.get_distribution(__package__).version
-
-else:
-    import importlib.metadata
-    __version__ = importlib.metadata.version(__package__)
